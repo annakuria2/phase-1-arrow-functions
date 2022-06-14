80acbd3e2f1776dc0eb968d54f8a82f5b055ad4d
Anna <annawkuria@gmail.com>
2022-06-14 12:49:31 +0300

    commit tree c4bc5781796ed608d6f67f35b283ef85421e10b3

---
+++ index.js
@@ -0,0 +1,8 @@
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(2,3); //=> 5
+const square =(parameter1) => parameter1 * parameter1;
+square(2,2) ; //=> 4
+const divide =(parameter1 ,parameter2) => 2000/100;
+divide(2000,100); //=> 20
+
+
